1ad614d4454a706a80e84be9fe6b085ff586d234
bensheridanedwards <bensheridanedwards@gmail.com>
2020-05-15 16:01:30 +0100

    feat: make api request into a async action creator with redux thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,8 @@
 import axios from 'axios'
 
-export const fetchPosts = async () => {
-  const baseURL = "https://jsonplaceholder.typicode.com"
-  const response = await axios.get(baseURL + '/posts');
-  
-  return {
-    type: 'FETCH_POSTS',
-    payload: response
+export const fetchPosts = () => async dispatch => {
+    const baseURL = "https://jsonplaceholder.typicode.com"
+    const response = await axios.get(baseURL + '/posts');
+
+    dispatch({ type: 'FETCH_POSTS', payload: response })
   };
-}
