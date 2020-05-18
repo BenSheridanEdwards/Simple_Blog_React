64e814ea843ccb1f9ef50577476076a488032ef3
bensheridanedwards <bensheridanedwards@gmail.com>
2020-05-18 15:05:20 +0100

    feat: return response data

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,5 +4,5 @@ export const fetchPosts = () => async dispatch => {
     const baseURL = "https://jsonplaceholder.typicode.com"
     const response = await axios.get(baseURL + '/posts');
 
-    dispatch({ type: 'FETCH_POSTS', payload: response })
+    dispatch({ type: 'FETCH_POSTS', payload: response.data })
   };
