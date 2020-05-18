eee300c8e9b21512e9bd9d2c19443e4e88148c5e
bensheridanedwards <bensheridanedwards@gmail.com>
2020-05-18 15:46:09 +0100

    feat: map and render posts list data as context with an icon, title and body

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -7,6 +7,22 @@ class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
+
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      )
+    })
+  }
   
   render() {
     console.log(this.props.posts);
