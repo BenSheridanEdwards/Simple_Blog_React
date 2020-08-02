793e320cee53474a10018b612a7fb9bd0ae32d81
bensheridanedwards <bensheridanedwards@gmail.com>
2020-08-02 19:32:39 +0100

    Improve: Clean up and delete unneded imports

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
-import { findByTestAttr } from '../test/testUtils';
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr } from "../test/testUtils";
 
-import App from './App';
+import App from "./App";
 
-const setup = (props={}, state=null) => {
-  const wrapper = shallow(<App {...props} />)
-  if (state) wrapper.setState(state)
-    return wrapper;
-}
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper;
+};
 
-it('renders app component without error', () =>{
+it("renders app component without error", () => {
   const wrapper = setup();
-  const appComponent = findByTestAttr(wrapper, 'component-app');
+  const appComponent = findByTestAttr(wrapper, "component-app");
   expect(appComponent.length).toBe(1);
 });
 
 it("renders a header with the text 'Simple Blog'", () => {
   const wrapper = setup();
-  const header = findByTestAttr(wrapper, 'app-header');
-  expect(header.text()).toMatch('Simple Blog')
-})
+  const header = findByTestAttr(wrapper, "app-header");
+  expect(header.text()).toMatch("Simple Blog");
+});
